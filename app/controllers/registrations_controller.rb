faa30ddd560a607b7aeaa24610e85622b22a6cd6
Ashish <asharma.ror@gmail.com>
2013-10-08 14:46:19 +0530

    Show image uploader error message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,9 +6,13 @@ class RegistrationsController < Devise::RegistrationsController
       params[:user].delete("password")
       params[:user].delete("password_confirmation")
       params[:user].delete("current_password")
-      current_user.update_attributes(params[:user])
-      flash[:notice] = "Image uploaded successfully."
-      redirect_to edit_user_registration_path and return
+      if current_user.update_attributes(params[:user])
+        flash[:notice] = "Image uploaded successfully."
+        redirect_to edit_user_registration_path and return
+      else
+        flash[:error] = current_user.errors.full_messages
+        render :edit
+      end
     else
       super
     end
